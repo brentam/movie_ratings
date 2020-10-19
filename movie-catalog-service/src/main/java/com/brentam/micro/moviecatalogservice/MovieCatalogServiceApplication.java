d9dcd6cee6529f033dd6d687f7b69898d4121f05
odair <brentam@gmail.com>
2020-10-19 22:22:34 +1100

    test2

--- movie-catalog-service/src/main/java/com/brentam/micro/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/brentam/micro/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
-//todo
+//todo2
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
